1c6f9783ce5002f6dbfa9718056725eb0dc3d16a
TJ <tijojose27@yahoo.com>
2018-12-16 00:05:24 -0600

    ADDED BUTTON AND SOME OTHER UI STUFF TO BEING THE SCORE APP

--- lib/main.dart
+++ lib/main.dart
@@ -3,59 +3,57 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      title: ("Basic App"),
+      title: ("Score App"),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic App with Text and button"),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: CustomTextView(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: CustomButton(),
-                )
-              ],
+        appBar: AppBar(
+          title: Text("Score App"),
+          centerTitle: true,
+        ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Score1Button(), Foul1Button()],
+              ),
             ),
-          )),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Score1Button(), Foul1Button()],
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
   );
 }
 
-class CustomTextView extends StatelessWidget {
+class Score1Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
-          hintText: "Please enter search here"),
-      textAlign: TextAlign.center,
-      textDirection: TextDirection.ltr,
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: () {},
-      color: Colors.lightBlue,
-      elevation: 6.0,
-      splashColor: Colors.blueGrey,
-      shape: new RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30.0)
-      ),
-      child: Text("Click To Clear Text !!", style: TextStyle(
-        fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white
-      ),),
+    return Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Container(
+            width: 230.0,
+            height: 50.0,
+            child: RaisedButton(
+              onPressed: () {},
+              color: Colors.lightBlue,
+              elevation: 6.0,
+              splashColor: Colors.blueGrey,
+              shape: new RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              child: Text(
+                "Goal",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            )
+        )
     );
   }
 }
